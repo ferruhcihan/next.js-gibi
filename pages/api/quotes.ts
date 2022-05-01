c2981efbb5dc747ad6e70e1088dfe28dd53177fc
ferruh <ferruhcihan20@gmail.com>
2022-05-01 05:59:50 +0300

    first commit

---
+++ pages/api/quotes.ts
@@ -0,0 +1,15 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  quote: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res
+    .status(200)
+    .json({ quote: '"Belki de bu yüzden yalnızca cesurların işidir badana"' });
+}
